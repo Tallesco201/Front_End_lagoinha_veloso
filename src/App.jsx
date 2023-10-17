70078bfb64cf464487ebe2fa90c25045696c96e7
Talles Simao <102775805+Tallesco201@users.noreply.github.com>
2023-10-17 15:00:39 -0300

    Instalei as bibliotecas no package.json e criei a rota de registro e o componente header, alem de ter criado o a pasta components e router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-
+import "./App.css"
+import { RoutesMain } from "./Routes/index.routes.";
 
 function App() {
   return (
     <div className="App">
-     
+      <RoutesMain/>
     </div>
   );
 }
